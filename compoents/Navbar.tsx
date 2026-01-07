a5336fc085e2beab8886ed22e6ef56274e1753fd
Georgewill Hope <georgeblue222@gmail.com>
2026-01-07 05:12:46 +0100

    I create a new next app. After that i created a component folder and created three components inside of it (Navbar, Footer and Hero). Thereafter i developed the navbar and also made it responsive for all devices. lastly i Made use of the footer and navbar components inside of my layout component to enable my navbar and footer show in all pages.

---
+++ compoents/Navbar.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import Image from "next/image";
+import logo from "@/public/assets/logo.png";
+import { navData } from "@/utils/data";
+import Link from "next/link";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [openNavbar, setOpenNavbar] = useState(false);
+  return (
+    <nav className="w-full lg:h-20 backdrop-blur-3xl sticky top-0 z-20 bg-black">
+      {/* desktops and laptops navbar */}
+      <div className="hidden container_width lg:flex  items-center justify-between">
+        <div>
+          {/* navbar logo */}
+          <Link href="/">
+            <Image
+              src={logo}
+              alt="logo"
+              width={100}
+              height={50}
+              objectFit="contain"
+              objectPosition="center"
+              className="rounded"
+            />
+          </Link>
+        </div>
+
+        {/* navbar links */}
+        <ul className="flex items-center justify-center gap-5">
+          {navData.map((item) => (
+            <li
+              key={item.id}
+              className="font-mono text-sm tracking-wider text-white"
+            >
+              <Link href={item.href}>{item.title}</Link>
+            </li>
+          ))}
+          <li className="font-mono text-[10px] tracking-wider">
+            +234 8051073722
+          </li>
+        </ul>
+      </div>
+
+      {/* mobile phones and tablet navbar */}
+
+      <div className="lg:hidden  items-center justify-between bg-black">
+        <div className="flex items-center justify-between h-16 container_width">
+          {/* Navbar logo */}
+          <Link href="/">
+            <Image
+              src={logo}
+              alt="logo"
+              width={80}
+              height={50}
+              objectFit="contain"
+              objectPosition="center"
+              className="rounded"
+            />
+          </Link>
+
+          <button onClick={() => setOpenNavbar(!openNavbar)}>
+            {!openNavbar ? (
+              <AiOutlineMenu size={25} />
+            ) : (
+              <AiOutlineClose size={25} />
+            )}
+          </button>
+        </div>
+
+        <div
+          className={`absolute h-[calc(100vh-64px)] w-full bg-black z-50 transition-all duration-700 ${
+            openNavbar
+              ? "translate-x-0"
+              : "-translate-x-125 md:-translate-x-225"
+          }`}
+        >
+          {/* navbar links */}
+          <ul className="w-full flex flex-col pt-10 gap-7 container_width">
+            {navData.map((item) => (
+              <li
+                key={item.id}
+                className="font-mono text-sm tracking-wider text-white"
+              >
+                <Link href={item.href}>{item.title}</Link>
+              </li>
+            ))}
+            <li className="font-mono text-[10px] tracking-wider mt-auto mb-5">
+              +234 8051073722
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ utils/data.ts
@@ -0,0 +1,38 @@
+interface navDataType {
+  id: number;
+  title: string;
+  href: string;
+}
+
+export const navData: navDataType[] = [
+  {
+    id: 1,
+    title: "Home",
+    href: "#&",
+  },
+  {
+    id: 2,
+    title: "About",
+    href: "#&",
+  },
+  {
+    id: 3,
+    title: "Classes",
+    href: "#&",
+  },
+  {
+    id: 4,
+    title: "Trainers",
+    href: "#&",
+  },
+  {
+    id: 5,
+    title: "Testimonials",
+    href: "#&",
+  },
+  {
+    id: 6,
+    title: "Contact",
+    href: "#&",
+  },
+];
