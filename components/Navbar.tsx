08a3ff5bb33da8fcded54f463245e32c526823cf
Georgewill-Hope <137285496+Georgewill-Hope@users.noreply.github.com>
2026-01-12 16:20:52 +0100

    Increase z-index for Navbar elements

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,7 +10,7 @@ import { useState } from "react";
 const Navbar = () => {
   const [openNavbar, setOpenNavbar] = useState(false);
   return (
-    <nav className="w-full lg:h-20 backdrop-blur-3xl sticky top-0 transition-allsition-all bg-linear-60 from-amber-950/20 via-gray-50/10 to-amber-950/20">
+    <nav className="w-full lg:h-20 backdrop-blur-3xl sticky top-0 z-50 bg-linear-60 from-amber-950/20 via-gray-50/10 to-amber-950/20">
       {/* desktops and laptops navbar */}
       <div className="hidden container_width md:flex items-center justify-between">
         <div>
@@ -71,7 +71,7 @@ const Navbar = () => {
         </div>
 
         <div
-          className={`absolute h-[calc(100vh-64px)] w-full z-10 transition-all duration-500 bg-orange-600 ${
+          className={`absolute h-[calc(100vh-64px)] w-full z-50 transition-all duration-500 bg-orange-600 ${
             openNavbar ? "translate-x-0" : "-translate-x-250"
           }`}
         >
